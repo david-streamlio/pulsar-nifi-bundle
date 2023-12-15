9be965a2d918daf658b5a998406d1b0e6292aabd
Umar Cheema <umar.cheema@chase.io>
2023-12-15 00:07:27 +0000

    added newer implementation of consumeMessages

--- nifi-pulsar-processors/src/test/java/org/apache/nifi/processors/pulsar/pubsub/TestConsumePulsarRecord.java
+++ nifi-pulsar-processors/src/test/java/org/apache/nifi/processors/pulsar/pubsub/TestConsumePulsarRecord.java
@@ -16,18 +16,7 @@
  */
 package org.apache.nifi.processors.pulsar.pubsub;
 
-import static org.apache.nifi.processors.pulsar.pubsub.ConsumePulsarRecord.RECORD_READER;
-import static org.apache.nifi.processors.pulsar.pubsub.ConsumePulsarRecord.RECORD_WRITER;
-
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
+import org.apache.nifi.flowfile.FlowFile;
 import org.apache.nifi.processors.pulsar.AbstractPulsarConsumerProcessor;
 import org.apache.nifi.processors.pulsar.AbstractPulsarProcessorTest;
 import org.apache.nifi.processors.pulsar.pubsub.mocks.MockRecordParser;
@@ -43,6 +32,17 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
+
+import static org.apache.nifi.processors.pulsar.pubsub.ConsumePulsarRecord.RECORD_READER;
+import static org.apache.nifi.processors.pulsar.pubsub.ConsumePulsarRecord.RECORD_WRITER;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.*;
+
 public class TestConsumePulsarRecord extends AbstractPulsarProcessorTest<byte[]> {
 
     protected static String BAD_MSG = "Malformed message";
@@ -57,7 +57,7 @@ public class TestConsumePulsarRecord extends AbstractPulsarProcessorTest<byte[]>
     protected MockRecordWriter writerService;
 
     @SuppressWarnings("unchecked")
-	@Before
+    @Before
     public void setup() throws InitializationException {
 
         mockMessage = mock(Message.class);
@@ -92,42 +92,42 @@ public class TestConsumePulsarRecord extends AbstractPulsarProcessorTest<byte[]>
     }
 
     protected void doFailedParseHandlingTest(String msg, String topic, String sub, boolean async) throws Exception {
-    	final String failingReaderId = "failing-record-reader";
-    	readerService = new MockRecordParser(-1);
-    	readerService.addSchemaField("name",  RecordFieldType.STRING);
-    	readerService.addSchemaField("age",  RecordFieldType.INT);
-    	runner.addControllerService(failingReaderId, readerService);
-    	runner.enableControllerService(readerService);
-    	
-    	runner.setProperty(RECORD_READER, failingReaderId);
-    	
-    	runner.setProperty(ConsumePulsarRecord.ASYNC_ENABLED, Boolean.toString(async));
-    	runner.setProperty(ConsumePulsarRecord.TOPICS, topic);
-    	runner.setProperty(ConsumePulsarRecord.SUBSCRIPTION_NAME, sub);
-    	runner.setProperty(ConsumePulsarRecord.CONSUMER_BATCH_SIZE, 2 + "");
-    	runner.setProperty(ConsumePulsarRecord.MESSAGE_DEMARCATOR, "***");
-    	runner.setProperty(ConsumePulsar.SUBSCRIPTION_TYPE, "Exclusive");
-    	
-    	if (async) {
-    		runner.setProperty(ConsumePulsarRecord.MAX_WAIT_TIME, "5 sec");
-    	} else {
-    		runner.setProperty(ConsumePulsarRecord.MAX_WAIT_TIME, "0 sec");
-    	}
-    	
-    	when(mockMessage.getData()).thenReturn(msg.getBytes());
+        final String failingReaderId = "failing-record-reader";
+        readerService = new MockRecordParser(-1);
+        readerService.addSchemaField("name", RecordFieldType.STRING);
+        readerService.addSchemaField("age", RecordFieldType.INT);
+        runner.addControllerService(failingReaderId, readerService);
+        runner.enableControllerService(readerService);
+
+        runner.setProperty(RECORD_READER, failingReaderId);
+
+        runner.setProperty(ConsumePulsarRecord.ASYNC_ENABLED, Boolean.toString(async));
+        runner.setProperty(ConsumePulsarRecord.TOPICS, topic);
+        runner.setProperty(ConsumePulsarRecord.SUBSCRIPTION_NAME, sub);
+        runner.setProperty(ConsumePulsarRecord.CONSUMER_BATCH_SIZE, 2 + "");
+        runner.setProperty(ConsumePulsarRecord.MESSAGE_DEMARCATOR, "***");
+        runner.setProperty(ConsumePulsar.SUBSCRIPTION_TYPE, "Exclusive");
+
+        if (async) {
+            runner.setProperty(ConsumePulsarRecord.MAX_WAIT_TIME, "5 sec");
+        } else {
+            runner.setProperty(ConsumePulsarRecord.MAX_WAIT_TIME, "0 sec");
+        }
+
+        when(mockMessage.getData()).thenReturn(msg.getBytes());
         when(mockMessage.getTopicName()).thenReturn("foo");
 
         mockClientService.setMockMessage(mockMessage);
-    	
-    	runner.run();
-    	runner.assertAllFlowFilesTransferred(ConsumePulsarRecord.REL_PARSE_FAILURE);
-    	runner.assertQueueEmpty();
-    	
-    	List<MockFlowFile> flowFiles = runner.getFlowFilesForRelationship(ConsumePulsarRecord.REL_PARSE_FAILURE);
-    	assertEquals(1, flowFiles.size());
-    	flowFiles.get(0).assertContentEquals(msg + "***" + msg);
+
+        runner.run();
+        runner.assertAllFlowFilesTransferred(ConsumePulsarRecord.REL_PARSE_FAILURE);
+        runner.assertQueueEmpty();
+
+        List<MockFlowFile> flowFiles = runner.getFlowFilesForRelationship(ConsumePulsarRecord.REL_PARSE_FAILURE);
+        assertEquals(1, flowFiles.size());
+        flowFiles.get(0).assertContentEquals(msg + "***" + msg);
     }
-    
+
     protected List<MockFlowFile> sendMessages(String msg, boolean async, int iterations) throws PulsarClientException {
         return sendMessages(msg, DEFAULT_TOPIC, DEFAULT_SUB, async, iterations, 1);
     }
@@ -141,9 +141,9 @@ public class TestConsumePulsarRecord extends AbstractPulsarProcessorTest<byte[]>
     }
 
     protected List<MockFlowFile> sendMessages(String msg, String topic, String sub, boolean async, int iterations, int batchSize) throws PulsarClientException {
-    	return sendMessages(msg, topic, sub, async, iterations, batchSize, "Exclusive");
+        return sendMessages(msg, topic, sub, async, iterations, batchSize, "Exclusive");
     }
-    
+
     protected List<MockFlowFile> sendMessages(String msg, String topic, String sub, boolean async, int iterations, int batchSize, String subType) throws PulsarClientException {
         when(mockMessage.getData()).thenReturn(msg.getBytes());
         when(mockMessage.getTopicName()).thenReturn(topic);
@@ -154,11 +154,11 @@ public class TestConsumePulsarRecord extends AbstractPulsarProcessorTest<byte[]>
         runner.setProperty(ConsumePulsarRecord.SUBSCRIPTION_NAME, sub);
         runner.setProperty(ConsumePulsarRecord.CONSUMER_BATCH_SIZE, batchSize + "");
         runner.setProperty(ConsumePulsar.SUBSCRIPTION_TYPE, subType);
-        
+
         if (async) {
-          runner.setProperty(ConsumePulsarRecord.MAX_WAIT_TIME, "5 sec");
+            runner.setProperty(ConsumePulsarRecord.MAX_WAIT_TIME, "5 sec");
         } else {
-          runner.setProperty(ConsumePulsarRecord.MAX_WAIT_TIME, "0 sec");
+            runner.setProperty(ConsumePulsarRecord.MAX_WAIT_TIME, "0 sec");
         }
 
         runner.run(iterations, true);
@@ -170,43 +170,50 @@ public class TestConsumePulsarRecord extends AbstractPulsarProcessorTest<byte[]>
         verify(mockClientService.getMockConsumer(), times(iterations * batchSize)).receive(0, TimeUnit.SECONDS);
 
         boolean shared = isSharedSubType(subType);
-        
+
         if (shared) {
-        	if (async) {
-        		verify(mockClientService.getMockConsumer(), times(iterations * batchSize)).acknowledgeAsync(mockMessage);
-        	} else {
-        		verify(mockClientService.getMockConsumer(), times(iterations * batchSize)).acknowledge(mockMessage);
-        	}
+            if (async) {
+                verify(mockClientService.getMockConsumer(), times(iterations * batchSize)).acknowledgeAsync(mockMessage);
+            } else {
+                verify(mockClientService.getMockConsumer(), times(iterations * batchSize)).acknowledge(mockMessage);
+            }
+        } else {
+            if (async) {
+                verify(mockClientService.getMockConsumer(), times(iterations)).acknowledgeCumulativeAsync(mockMessage);
+            } else {
+                verify(mockClientService.getMockConsumer(), times(iterations)).acknowledgeCumulative(mockMessage);
+            }
         }
-        else {
-        	if (async) {
-        		verify(mockClientService.getMockConsumer(), times(iterations)).acknowledgeCumulativeAsync(mockMessage);
-        	} else {
-        		verify(mockClientService.getMockConsumer(), times(iterations)).acknowledgeCumulative(mockMessage);
-        	}
-        }
-        
+
         return flowFiles;
     }
 
     protected void doMappedAttributesTest() throws PulsarClientException {
-        when(mockMessage.getData()).thenReturn("A,10".getBytes())
-           .thenReturn("B,10".getBytes())
-           .thenReturn("C,10".getBytes())
-           .thenReturn("D,10".getBytes());
-        
-        when(mockMessage.getProperty("prop")).thenReturn(null)
-          .thenReturn(null)
-          .thenReturn("val")
-          .thenReturn("val");
-        
-        when(mockMessage.getKey()).thenReturn(null)
-          .thenReturn(null)
-          .thenReturn(null)
-          .thenReturn("K");
+        when(mockMessage.getData())
+                .thenReturn("A,10".getBytes())
+                .thenReturn("B,10".getBytes())
+                .thenReturn("C,10".getBytes())
+                .thenReturn("D,10".getBytes())
+                .thenReturn("A,10".getBytes())
+                .thenReturn("B,10".getBytes())
+                .thenReturn("C,10".getBytes())
+                .thenReturn("D,10".getBytes());
+
+
+        when(mockMessage.getProperty("prop"))
+                .thenReturn(null)
+                .thenReturn(null)
+                .thenReturn("val")
+                .thenReturn("val");
+
+        when(mockMessage.getKey())
+                .thenReturn(null)
+                .thenReturn(null)
+                .thenReturn(null)
+                .thenReturn("K");
 
         when(mockMessage.getTopicName()).thenReturn("foo");
-        
+
         mockClientService.setMockMessage(mockMessage);
 
         runner.setProperty(ConsumePulsar.MAPPED_FLOWFILE_ATTRIBUTES, "prop,key=__KEY__");
@@ -224,14 +231,28 @@ public class TestConsumePulsarRecord extends AbstractPulsarProcessorTest<byte[]>
         assertEquals(3, flowFiles.size());
 
         // first flow file should have A, second should have B
-        flowFiles.get(0).assertAttributeNotExists("prop");
-        flowFiles.get(0).assertAttributeNotExists("key");
-        flowFiles.get(0).assertContentEquals("\"A\",\"10\"\n\"B\",\"10\"\n");
-        flowFiles.get(1).assertAttributeEquals("prop", "val");
-        flowFiles.get(1).assertAttributeNotExists("key");
-        flowFiles.get(1).assertContentEquals("\"C\",\"10\"\n");
-        flowFiles.get(2).assertAttributeEquals("prop", "val");
-        flowFiles.get(2).assertAttributeEquals("key", "K");
-        flowFiles.get(2).assertContentEquals("\"D\",\"10\"\n");
+        Optional<MockFlowFile> flowFileWithA = flowFiles.stream().filter(item ->
+                item.getContent().contains("\"A\",\"10\"\n")
+        ).findFirst();
+        if (!flowFileWithA.isPresent()) {
+            fail("Flow file missing");
+        } else {
+            flowFileWithA.get().assertAttributeExists("prop");
+            flowFileWithA.get().assertAttributeExists("key");
+        }
+        Optional<MockFlowFile> flowFileWithBC = flowFiles.stream().filter(item -> item.getContent().contains("\"B\",\"10\"\n\"C\",\"10\"\n")).findFirst();
+        if (!flowFileWithBC.isPresent()) {
+            fail("Flow file missing");
+        } else {
+            flowFileWithBC.get().assertAttributeNotExists("prop");
+            flowFileWithBC.get().assertAttributeNotExists("key");
+        }
+        Optional<MockFlowFile> flowFileWithD = flowFiles.stream().filter(item -> item.getContent().contains("\"D\",\"10\"\n")).findFirst();
+        if (!flowFileWithD.isPresent()) {
+            fail("Flow file missing");
+        } else {
+            flowFileWithD.get().assertAttributeEquals("prop", "val");
+            flowFileWithD.get().assertAttributeNotExists("key");
+        }
     }
 }
